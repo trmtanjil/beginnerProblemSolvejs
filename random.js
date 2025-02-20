fecbd146510f1711bd0526464916cd5a6945793f
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-02-20 16:42:21 +0600

    random number commit

--- random.js
+++ random.js
@@ -1,1 +1,6 @@
-console.log('kslj');
+function getRandom(min, max){
+    return Math.floor(Math.random() * (max - min + 1 ) +min);
+    //?max - min + 1 = 10 - 5 + 1 = 6
+    //?Math.random() * (max - min + 1);  <>  0.5 * 6 = 3
+}
+console.log(getRandom(5,10));
